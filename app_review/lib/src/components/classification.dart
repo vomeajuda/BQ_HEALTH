a2f74662c3381a89424ebd0cee2216f189ea4351
vomeajuda <leogugo9@hotmail.com>
2024-09-10 11:16:23 -0300

    Pequenas alteracoes

--- app_review/lib/src/components/classification.dart
+++ app_review/lib/src/components/classification.dart
@@ -1,15 +1,17 @@
 String classificarIMC(double imc) {
   if (imc < 18.5) { //verificacao de classificao do imc de acordo com o numero do imc
     return 'Abaixo do Peso';
-  } else if (imc >= 18.5 && imc <= 24.9) {
+  } else if (imc < 24.9) {
     return 'Peso Normal';
-  } else if (imc >= 25 && imc <= 29.9) {
+  } else if (imc < 29.9) {
     return 'Sobrepeso';
-  } else if (imc >= 30 && imc <= 34.9) {
+  } else if (imc < 34.9) {
     return 'Obesidade grau 1';
-  } else if (imc >= 35 && imc <= 39.9) {
+  } else if (imc < 39.9) {
     return 'Obesidade grau 2';
-  } else {
+  } else if (imc >= 40){
     return 'Obesidade grau 3';
+  } else {
+    return 'Ta morto';
   }
 }
